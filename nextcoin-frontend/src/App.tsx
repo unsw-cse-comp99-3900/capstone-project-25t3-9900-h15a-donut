076af8277c28e492a3b12c578373cbeade0ba9e8
zaynshen <lizhushen71010@gmail.com>
2025-10-16 13:01:21 +1100

    change icon

--- nextcoin-frontend/src/App.tsx
+++ nextcoin-frontend/src/App.tsx
@@ -1,11 +1,8 @@
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -18,41 +15,19 @@ function App() {
   }, [navigate]);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          minHeight: "100vh",
-          background: "linear-gradient(to bottom, #a2c8ff, #f9fbff)",
-        }}
-      >
-        <h1 style={{ fontSize: "3rem", color: "#0f172a" }}>
-          Welcome to NextCoin
-        </h1>
-      </div>
-    </>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        minHeight: "100vh",
+        background: "linear-gradient(to bottom, #a2c8ff, #f9fbff)",
+      }}
+    >
+      <h1 style={{ fontSize: "3rem", color: "#0f172a" }}>
+        Welcome to NextCoin
+      </h1>
+    </div>
   );
 }
 
